545001b27aa3db72013e29016eec2647a308dc97
Alejandro-Valle <143530888+Alejandro-Valle@users.noreply.github.com>
2023-09-05 09:52:49 -0600

    L1_GAVB1000623

---
+++ L1_GAVB1000623.py
@@ -0,0 +1,34 @@
+
+#variable1=21000
+#print(variable1)
+#print(type(variable1))
+
+#variable1 = "Hola"
+#print(variable1)
+#print(type(variable1))
+
+#variable2 = 12.25
+#print(variable2)
+#print(type(variable2))
+
+#variable2 = True
+#print(variable2)
+#print(type(variable2), end="") 
+
+#print("Hola mundo soy Alejandro")
+
+#print("Hola mundo") 
+#print("Soy Alejandro")
+
+    #La diferencia entre "print" y "print, end=" es que en la primera hará un salto de linea cuando termine el enunciado, cuando agregamos "end=" no hay ningun salto de linea y escribirá todo seguido
+
+#print("Hola mundo", end="") 
+#print(" Soy Alejandro", end="")
+
+#texto= input()
+#print(texto+" Hola")
+
+print("Ingrese su nombre")
+nombre= input()
+print("Hola mundo, soy " + nombre)
+
