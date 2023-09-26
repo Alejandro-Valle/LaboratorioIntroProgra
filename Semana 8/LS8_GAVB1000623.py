1cc5586a8b89825f806364fa0bb521ecd4abbcda
Alejandro-Valle <143530888+Alejandro-Valle@users.noreply.github.com>
2023-09-26 06:02:37 -1100

    Add files via upload

--- Semana 8/LS8_GAVB1000623.py
+++ Semana 8/LS8_GAVB1000623.py
@@ -1,25 +1,35 @@
-#Alejandro Valle 
+#Introducción a la programación
+#Alejandro Valle 1000623
+#26/09/2023
 
 print("Seleccione una de las siguientes opciones: 1. Factorial, 2. Secuencia de Fibonacci, 3. Salir")
 operacion=input()
 
-if operacion == "1":
-    print("ingrese un valor")
-    x=int(input(), base=0)
-    print("El factorial de dicho número es: ")
-    for p in range (x):
-        resultado=(p-1)*(p-2)
-        print(resultado)
+while operacion !="3":
+    if operacion == "1":
+        print("ingrese un valor")
+        x=int(input(), base=0)
+        factorial=1
+        print("El factorial de dicho número es: ")
+        for p in range (1,x+1):
+            factorial= factorial * p
+            print(factorial)
 
 
-if operacion =="2":
-    print("ingrese un valor")
-    y=int(input(), base=0)
-    print("Fibonacci: ")
-    for z in range (y):
-        resultado=(z-1)+(z-2)
-        print(resultado)
+    if operacion =="2":
+        print("ingrese un valor")
+        y=int(input(), base=0)
+        print("Fibonacci: ")
+        a=0
+        b=1
+        for z in range (y):
+            c=b+a
+            a=b
+            b=c
+            print(a)
 
-if operacion == "3":
-    print("Ha salido del menú")
+    if operacion == "3":
+        print("Ha salido del menú")
+    
+
         
